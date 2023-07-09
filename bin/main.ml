ac6134155958917431af5a08db2541a567e00300
Steve Purcell <steve@sanityinc.com>
2023-07-09 14:34:45 +0200

    Add volumes

--- bin/main.ml
+++ bin/main.ml
@@ -27,8 +27,10 @@ type problem = { room_width : int;
 
 type placement = { x: int; y: int } [@@deriving yojson_of];;
 
-type solution = { placements: placement list } [@@deriving yojson_of]
 
+type solution = { placements: placement list;
+                  volumes: int list;
+                } [@@deriving yojson_of]
 
 (* Make a set of potential positions by using staggered rows *)
 (* of points within the stage space inset from the edge *)
@@ -63,10 +65,11 @@ let raw_score_at instrument attendees placement =
     ) attendees;
   !total;;
 
+let num_instrs p = 1 + Array.fold_left max 0 p.musicians;;
+
 let choose_best p positions =
   let positions = Array.of_list positions in
-  let num_instrs = 1 + Array.fold_left max 0 p.musicians in
-  let instrument_counts = Array.make num_instrs 0 in
+  let instrument_counts = Array.make (num_instrs p) 0 in
   Array.iter (fun i -> Array.set instrument_counts i (1 + Array.get instrument_counts i)) p.musicians;
 
   let scored_positions =
@@ -81,13 +84,14 @@ let choose_best p positions =
   (* Mutable state while assigning places *)
   let best_positions = ref (List.sort (fun (_, _, s1) (_, _, s2) -> Float.compare s2 s1) scored_positions) in
   let placements_by_position_idx = Array.map (fun _ -> None) positions in
-  let placements_by_instrument = Array.init num_instrs (fun _ -> Queue.create ()) in
+  let placements_by_instrument = Array.init (num_instrs p) (fun _ -> Queue.create ()) in
+  let volumes_by_position_idx = Array.map (fun _ -> 10) positions in
 
   let placed_count = ref 0 in
   while !placed_count < Array.length p.musicians do
     match !best_positions with
     | [] -> failwith "ran out of positions!";
-    | ((instrument, position_idx, _score) :: rest) ->
+    | ((instrument, position_idx, score) :: rest) ->
       best_positions := rest;
       match Array.get placements_by_position_idx position_idx with
       | None -> begin
@@ -96,16 +100,27 @@ let choose_best p positions =
           | remaining_players ->
             Array.set placements_by_position_idx position_idx (Some instrument);
             Array.set instrument_counts instrument (remaining_players - 1);
-            Queue.push (Array.get positions position_idx) (Array.get placements_by_instrument instrument);
+            Queue.push position_idx (Array.get placements_by_instrument instrument);
+            Array.set volumes_by_position_idx position_idx (if score > 0.0 then 10 else 0);
             placed_count := !placed_count + 1
         end
       | _ -> (); (* Spot already filled *)
   done;
 
-  Array.map (fun i -> Array.get placements_by_instrument i |> Queue.pop) p.musicians |> Array.to_list;;
+  (* list of placement, volume *)
+  Array.map
+    (fun i ->
+       let position_idx = Array.get placements_by_instrument i |> Queue.pop in
+       let volume = Array.get volumes_by_position_idx position_idx in
+       let position = Array.get positions position_idx in
+       (position, volume))
+    p.musicians |> Array.to_list;;
 
 let spread_solver p =
-  { placements = grid_positions p |> choose_best p };;
+  let placements_and_volumes = grid_positions p |> choose_best p in
+  { placements = List.map fst placements_and_volumes;
+    volumes = List.map snd placements_and_volumes;
+  };;
 
 let () =
   Random.init 42;
